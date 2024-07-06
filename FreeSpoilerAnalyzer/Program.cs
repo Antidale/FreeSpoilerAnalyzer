0a8838c523354c24eb083fd657fc83d76abba4e1
antidale <antidale@tellah.life>
2024-07-05 19:42:46 -0700

    Adding checks for folders and min file count

--- FreeSpoilerAnalyzer/Program.cs
+++ FreeSpoilerAnalyzer/Program.cs
@@ -2,6 +2,7 @@
 using FreeSpoilerAnalyzer.Enums;
 
 List<string> folders = [];
+FileInfo[] spoilerLogs = [];
 
 var undergroundCount = 0;
 var fileCount = 0;
@@ -15,19 +16,32 @@ foreach (var arg in args)
     }
 }
 
+if (folders.Count < 1)
+{
+    Console.WriteLine("No folders found");
+    return;
+}
+
 foreach (var folder in folders)
 {
     var directoryInfo = new DirectoryInfo(folder);
-    var spoilerLogs = directoryInfo.GetFiles(searchPattern: "*.spoiler.private");
-
-    foreach (var spoilerLog in spoilerLogs)
-    {
-        var keyItemPlacement = await SpoilerParser.ParseKeyItemPlacementAsync(spoilerLog);
-        if (SpoilerAnalyzer.IsViaUnderground(keyItemPlacement, KeyItem.DarknessCrystal)) { undergroundCount++; };
-        fileCount++;
-    }
+    spoilerLogs = [.. spoilerLogs, .. directoryInfo.GetFiles(searchPattern: "*.spoiler.private")];
 }
 
-Console.WriteLine($"Total Files: {fileCount}\r\nDarkness via Underground: {undergroundCount}\r\nPercentage: {100.0 * (double)undergroundCount/(double)fileCount}");
+if(spoilerLogs.Count() < 100)
+{
+    Console.WriteLine("Analyzer is for large amounts of files, please ensure you have at least 100");
+    return;
+}
+
+foreach (var spoilerLog in spoilerLogs)
+{
+    var keyItemPlacement = await SpoilerParser.ParseKeyItemPlacementAsync(spoilerLog);
+    if (SpoilerAnalyzer.IsViaUnderground(keyItemPlacement, KeyItem.DarknessCrystal)) { undergroundCount++; };
+    fileCount++;
+}
+
+
+Console.WriteLine($"Total Files: {fileCount}\r\nDarkness via Underground: {undergroundCount}\r\nPercentage: {100.0 * (double)undergroundCount / (double)fileCount}");
 Console.WriteLine("\r\nPress any key to close");
 Console.ReadKey();
