ff890c9a931e8be88a2ada7bf52a10a666674181
Antidale <antidale@tellah.life>
2025-04-16 16:15:16 -0700

    Do some spoiler parsing for tourney data

--- FreeSpoilerAnalyzer/Models/SpoilerMetadata.cs
+++ FreeSpoilerAnalyzer/Models/SpoilerMetadata.cs
@@ -1,8 +1,11 @@
-﻿namespace FreeSpoilerAnalyzer.Models
+﻿using SQLite;
+
+namespace FreeSpoilerAnalyzer.Models
 {
-    public class KeyItemSpoiler
+    public class SpoilerMetadata
     {
         public string Flagset { get; init; } = string.Empty;
+
         public string Seed { get; init; } = string.Empty;
 
     }
--- FreeSpoilerAnalyzer/SpoilerParser.cs
+++ FreeSpoilerAnalyzer/SpoilerParser.cs
@@ -2,25 +2,48 @@
 using FreeSpoilerAnalyzer.Enums;
 using FreeSpoilerAnalyzer.Extensions;
 using System.Text.RegularExpressions;
+using FreeSpoilerAnalyzer.Models;
+using KeyItemLocation = FreeSpoilerAnalyzer.Enums.KeyItemLocation;
 
 namespace FreeSpoilerAnalyzer
 {
     public partial class SpoilerParser
     {
+        public async Task<SpoilerMetadata> GetSpoilerMetadata(StreamReader streamReader)
+        {
+            var seed = "unknown";
+            var flagset = "unknown";
+
+            var currentLine = await AdvanceToSectionAsync(streamReader, SpoilerConstants.BinaryFlags);
+            
+            
+            var matches = SpoilerMetadataRegex().Match(currentLine);
+            if (matches.Success)
+            {
+                flagset = matches.Groups[2].Captures.FirstOrDefault()?.Value ?? flagset;
+            }
+            
+            currentLine = await AdvanceToSectionAsync(streamReader, SpoilerConstants.Seed);
+            matches = SpoilerMetadataRegex().Match(currentLine);
+            if (matches.Success)
+            {
+                seed = matches.Groups[2].Captures.FirstOrDefault()?.Value ?? seed;
+            }
+
+            return new SpoilerMetadata
+            {
+                Flagset = flagset,
+                Seed = seed,
+            };
+        }
+        
         public async Task<Dictionary<KeyItem, KeyItemLocation>> ParseKeyItemPlacementAsync(StreamReader streamReader)
         {
             //Default the tracking dictionary to have everything in the overworld
             var keyItems = Enum.GetValues<KeyItem>().Where(x => (int)x > -1).ToDictionary(x => x, y => KeyItemLocation.Starting);
             var keyItemLocationsMap = Enum.GetValues<KeyItemLocation>().ToDictionary(key => key.GetDescription(), value => value);
             
-
-            var currentLine = await streamReader.ReadLineAsync();
-
-            //Read to the header section for KI Locations
-            while (currentLine is not null && !currentLine.StartsWith(SpoilerConstants.KeyItemLocations))
-            {
-                currentLine = await streamReader.ReadLineAsync();
-            }
+            var currentLine = await AdvanceToSectionAsync(streamReader, SpoilerConstants.KeyItemLocations);
 
             //Read to the section divider, getting KI & World Pairings along the way
             while (currentLine is not null && !currentLine.StartsWith(SpoilerConstants.SectionDivider))
@@ -45,7 +68,21 @@ namespace FreeSpoilerAnalyzer
             return keyItems;
         }
 
+        private async Task<string> AdvanceToSectionAsync(StreamReader streamReader, string sectionStart)
+        {
+            var currentLine = await streamReader.ReadLineAsync();
+            while (currentLine is not null && !currentLine.StartsWith(sectionStart))
+            {
+                currentLine = await streamReader.ReadLineAsync();
+            }
+            
+            return currentLine ?? string.Empty;
+        }
+
         [GeneratedRegex(@"(^[\w\s\-\(\)\.?/']+)\s\.{2,}\s([\w\s-()/\.']+)", RegexOptions.IgnoreCase, "en-US")]
         public static partial Regex SpoilerKeyValueRegex();
+
+        [GeneratedRegex(@"(^[\w\:\']+)\s{2,}\s([\w]+)", RegexOptions.IgnoreCase, "en-US")]
+        public static partial Regex SpoilerMetadataRegex();
     }
 }
